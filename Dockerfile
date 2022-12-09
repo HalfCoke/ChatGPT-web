82d686d1284862603a6a638ed27e16a365b727e2
halfcoke <halfcoke@163.com>
2022-12-10 02:04:40 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:lts-alpine
+
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
+
+RUN apk add tzdata && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone && apk del tzdata
+
+ENV LANG zh_CN.UTF-8
+ENV LANGUAGE zh_CN.UTF-8
+ENV TZ Asia/Shanghai
+
+RUN apk add g++ make python3
+
+COPY . /app
+WORKDIR /app
+RUN npm install --registry=https://registry.npm.taobao.org
+
+CMD UV_THREADPOOL_SIZE=4 node /app/index.js
---
+++ index.js
@@ -0,0 +1,90 @@
+const config = {
+  ChatGPTSessionToken: process.env.GPT_TOKEN
+}
+
+function getConversation (contactId) {
+  if (conversationMap.has(contactId)) {
+    return conversationMap.get(contactId);
+  }
+  const conversation = chatGPT.getConversation();
+  conversationMap.set(contactId, conversation);
+  return conversation;
+}
+
+async function getChatGPTReply (MsgId, content, contactId) {
+  const currentConversation = getConversation(contactId);
+  console.log('MsgId: ' + MsgId + ' Content: ', content);
+  // send a message and wait for the response
+  const threeMinutesMs = 3 * 60 * 1000
+  const response = await pTimeout(
+      currentConversation.sendMessage(content),
+      {
+        milliseconds: threeMinutesMs,
+        message: 'ChatGPT timed out waiting for response'
+      }
+  )
+  console.log('MsgId: ' + MsgId + ' Response: ', response);
+  // response is a markdown-formatted string
+  return response
+}
+
+async function replyMessage (MsgId, content, contactId) {
+  try {
+    return await getChatGPTReply(MsgId, content, contactId)
+  } catch (e) {
+    console.error(e);
+    if (e.message.includes('timed out')) {
+      return 'Please try again, ChatGPT timed out waiting for response.';
+    }
+    conversationMap.delete(contactId);
+  }
+}
+
+function buildResponse (FromUserName, ToUserName, content) {
+  var xmlContent = '<xml><ToUserName><![CDATA[' + FromUserName + ']]></ToUserName>'
+  xmlContent += '<FromUserName><![CDATA[' + ToUserName + ']]></FromUserName>'
+  xmlContent += '<CreateTime>' + new Date().getTime() + '</CreateTime>'
+  xmlContent += '<MsgType><![CDATA[text]]></MsgType>'
+  xmlContent += '<Content><![CDATA[' + content + ']]></Content></xml>'
+  return xmlContent
+}
+
+
+const Koa = require('koa');
+const Router = require('@koa/router');
+const bodyParser = require('koa-bodyparser');
+const cors = require("koa2-cors");
+const conversationMap = new Map();
+const chatGPT = new ChatGPTAPI({ sessionToken: config.ChatGPTSessionToken });
+
+const app = new Koa();
+app.use(bodyParser());
+const router = new Router({
+  prefix: '/api/chat'
+});
+
+// 状态
+router.get('/', async ctx => {
+
+  let test = await status(ctx.params.action);
+  if (test) {
+    ctx.body = { status: 200 }
+  } else {
+    if (ctx.params.MsgType === "text") {
+      contactId = ctx.params.FromUserName
+      content = ctx.params.Content
+      MsgId = ctx.params.MsgId
+      console.log('MsgId: ' + MsgId + ' Talker: ' + contactId)
+      ctx.body = replyMessage(MsgId, content, contactId)
+    } else {
+      ctx.body = buildResponse(ctx.params.FromUserName, ctx.params.ToUserName, "请发送纯文本消息")
+    }
+  }
+});
+
+
+app.use(cors());
+app.use(router.routes());
+app.listen(8080);
+console.log('[' + new Date() + '] listening on http 8080');
+
